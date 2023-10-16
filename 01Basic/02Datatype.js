2d5aa5683c182b6bcd3f00e4505dba3591629246
ANURAG <87489250+nigdifg@users.noreply.github.com>
2023-10-16 04:18:34 +0000

    data typoes ADDED

---
+++ 01Basic/02Datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as a newer version
+
+// alert(2+3) //we are using node js not browser
+
+console.log(3+3)
+
+let name ="Anurag" //string datatype
+let age =21
+let isloggedin = false
+/*
+range
+number- >   2^53
+bigint
+string->  ""
+boolean->  true/false
+null ->  standalone value
+undefined-> vlaue not assigned
+symbol-> finding uniqueness 
+
+objects:
+
+*/
+
+console.log(typeof(undefined));  //undefined
+console.log(typeof(null));  //object

a675ca9684996742a7e7a1490c180450a5abc3f8
ANURAG <87489250+nigdifg@users.noreply.github.com>
2023-11-23 11:44:59 +0000

    IIFE completed!

--- 03Basic/03_arrow.js
+++ 03Basic/03_arrow.js
@@ -1,13 +1,18 @@
-const user = {
-    username: "hitesh",
+const userFunction = {
+    username: "Anurag",
     price: 999,
-
-    welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
+    greetMsg: function(){
+        console.log(`${this.username},welcomne to website!`);
         console.log(this);
     }
-
 }
+
+// console.log(userFunction.username);
+// userFunction.username = "MAurya"
+// console.log(userFunction.username);
+// userFunction.greetMsg();
+
+// console.log(userFunction.greetMsg());
 
 // user.welcomeMessage()
 // user.username = "sam"
@@ -16,24 +21,24 @@ const user = {
 // console.log(this);
 
 // function chai(){
-//     let username = "hitesh"
+//     let username = "Anurag"
 //     console.log(this.username);
 // }
 
 // chai()
 
 // const chai = function () {
-//     let username = "hitesh"
+//     let username = "Anurag"
 //     console.log(this.username);
 // }
 
 const chai =  () => {
-    let username = "hitesh"
+    let username = "ANurag"
     console.log(this);
 }
 
 
-// chai()
+chai()
 
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
@@ -43,10 +48,10 @@ const chai =  () => {
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+// const addTwo = (num1, num2) => ({username: "hitesh"})
 
 
-console.log(addTwo(3, 4))
+// console.log(addTwo(3, 4))
 
 
 // const myArray = [2, 5, 3, 7, 8]

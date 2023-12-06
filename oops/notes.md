a4cd268786f5b0f4c9f244ecb87ba8f70fc88567
ANURAG <87489250+nigdifg@users.noreply.github.com>
2023-12-06 05:36:04 +0000

    Oops completed! in JS

---
+++ oops/notes.md
@@ -0,0 +1,24 @@
+# jabcascript and classes
+
+## OOP
+
+## Objects:
+- collection of properties and methods
+- toLowerCase
+
+## why use OOP
+
+## Parts of OOP
+
+Object literal
+
+- Constructor function
+- Prototypes
+- Classes
+- Instances (new,this)
+
+## 4 pillears
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
---
+++ oops/oop.js
@@ -0,0 +1,31 @@
+const user ={
+    username: "Anurag",
+    loginCount : 8,
+    signedIn:true,
+
+    getUserDetails: function(){
+        console.log("Got user details from database");
+        console.log(`username: ${this.username}`);
+        console.log(this);
+    }
+}
+console.log(user.username);
+console.log(user.loginCount);
+console.log(user.signedIn);
+console.log(user.getUserDetails());
+
+function User(username,loginCount,isloggedin){
+    this.username =username;
+    this.loginCount = loginCount;
+    this.isloggedin = isloggedin;
+
+    this.greeting = function(){
+        console.log(`Welcomr ${this.username}`);
+    }
+    return this
+}
+
+const userOne = new User("Anurag",12,true)
+const userTwo = new User("ChaiAurCode",11,false)
+console.log(userOne.constructor);
+console.log(userTwo.constructor);
